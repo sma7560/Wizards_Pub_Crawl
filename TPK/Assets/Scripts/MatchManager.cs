a64f2afc522012cc78b1df45f073e7375b4d04da
Ethan Chan <chaneh@mcmaster.ca>
2018-10-24 20:03:54 -0400

    Created files in starting to make sides and implementing with networking

---
+++ TPK/Assets/Scripts/MatchManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MatchManager : MonoBehaviour {
+    private int maxAttackers = 3;
+
+    public int currentAttacker;
+    public bool defenderExist;
+
+
+	// Use this for initialization
+	void Start () {
+        currentAttacker = 0;
+        defenderExist = false;
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    // Return a boolean based on success of addition.
+    public bool AddDefender() {
+        if (defenderExist) {
+            return false;
+        }
+        return defenderExist = true;
+        
+    }
+
+    public bool AddAttacker() {
+        if (currentAttacker < maxAttackers)
+        {
+            currentAttacker++;
+            return true;
+        }
+        return false;
+    }
+}
---
+++ TPK/Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerManager : NetworkBehaviour {
+    // Enum for denoting player type
+    public enum PlayerType { Attacker, Defender};
+
+    private GameObject gameManager;
+    private Canvas initialGui;
+
+    public GameObject AttackerObject;
+    public GameObject DefenderObject;
+    public PlayerType myType;
+
+
+	// Use this for initialization
+	void Start () {
+        initialGui = GameObject.Find("InitialGui").GetComponent<Canvas>();
+        initialGui.enabled = true;
+        // Need to figure out how to set the type up externally
+        //myType = PlayerType.Attacker;
+
+        //Instantiate(AttackerObject);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SetToAttacker() {
+        
+        myType = PlayerType.Attacker;
+        gameManager = GameObject.Find("GameManager");
+        Debug.Log(gameManager.name);
+
+        gameManager.GetComponent<MatchManager>().AddAttacker();
+        Debug.Log("Added an Attacker");
+        Instantiate(AttackerObject);
+
+
+        initialGui = GameObject.Find("InitialGui").GetComponent<Canvas>();
+        initialGui.enabled = false;
+
+    }
+
+    public void SetToDefender()
+    {
+
+        myType = PlayerType.Defender;
+        gameManager = GameObject.Find("GameManager");
+        Debug.Log(gameManager.name);
+
+
+        if (gameManager.GetComponent<MatchManager>().AddDefender()) {
+            Debug.Log("Added a Defender");
+            Instantiate(DefenderObject);
+        }
+        Debug.Log("Could not a Defender");
+
+        initialGui = GameObject.Find("InitialGui").GetComponent<Canvas>();
+        initialGui.enabled = false;
+
+    }
+}
