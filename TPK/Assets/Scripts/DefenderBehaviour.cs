05746cbdcf59ca8b58fb745d66b60127075229d6
Simon Ma <mas23@mcmaster.ca>
2018-10-26 16:38:33 -0400

    Starting work on monster spawning. SM

---
+++ TPK/Assets/Scripts/DefenderBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefenderBehaviour : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//Defender Monster spawn script
+		//Uses raycasting to select a tile to place a monster
+
+
+	}
+}
