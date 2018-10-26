45b5e41ca898f468ff3287c1872f04a1c86ae8c1
Simon Ma <mas23@mcmaster.ca>
2018-10-26 18:37:03 -0400

    Created basic monster/trap spawning. SM

--- TPK/Assets/Scripts/DefenderBehaviour.cs
+++ TPK/Assets/Scripts/DefenderBehaviour.cs
@@ -4,9 +4,21 @@ using UnityEngine;
 
 public class DefenderBehaviour : MonoBehaviour {
 
+	public enum defenderMode {spawnMonster, spawnTrap};
+
+	public GameObject monster;
+	public GameObject trap;
+	public int energy;
+	public int money;
+
+	public defenderMode mode;
+
 	// Use this for initialization
 	void Start () {
-		
+		energy = 1000;
+		money = 1000;
+
+		mode = defenderMode.spawnMonster;
 	}
 	
 	// Update is called once per frame
@@ -15,6 +27,31 @@ public class DefenderBehaviour : MonoBehaviour {
 		//Defender Monster spawn script
 		//Uses raycasting to select a tile to place a monster
 
+		if (Input.GetMouseButtonUp (0)) {
+			RaycastHit hit;
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
 
+			if (Physics.Raycast (ray, out hit, 200.0f) && hit.transform.tag == "Tile") {
+				switch (mode) {
+				case defenderMode.spawnMonster:
+					Instantiate (monster, hit.transform);
+					energy -= 100;
+					break;
+				case defenderMode.spawnTrap:
+					Instantiate (trap, hit.transform);
+					money -= 100;
+					break;
+				}
+			}
+			Debug.Log ("raycast hit " + hit.transform.name);
+		}
+
+		//switch spawn modes
+		if (Input.GetKeyUp (KeyCode.E)) {
+			if (mode == defenderMode.spawnMonster)
+				mode = defenderMode.spawnTrap;
+			else if (mode == defenderMode.spawnTrap)
+				mode = defenderMode.spawnMonster;
+		}
 	}
 }
