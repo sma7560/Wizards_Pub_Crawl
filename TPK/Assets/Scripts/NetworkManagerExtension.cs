b1a3e62cfa08f391b76f521554669e2313cbc6a3
Ethan Chan <chaneh@mcmaster.ca>
2018-10-26 16:34:40 -0400

    Began working on custom ui for networking as well as scripting it

---
+++ TPK/Assets/Scripts/NetworkManagerExtension.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class NetworkManagerExtension : NetworkManager{
+
+    public void StartUpHost() {
+
+    }
+
+    public void JoinGame() {
+    }
+
+    private void SetIPAddress() {
+
+    }
+
+    private void SetPort()
+    {
+
+    }
+}
