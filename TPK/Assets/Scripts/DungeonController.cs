8de98be231dad991137e35205ba19e0ecb124369
SusanSuY <yuens2@mcmaster.ca>
2018-10-27 13:57:42 -0400

    Add in-game menu to dungeon level scene. Add some functionality to in-game menu quit option

---
+++ TPK/Assets/Scripts/DungeonController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+// DungeonController: used to update dungeon status during dungeon level gameplay
+
+public class DungeonController : MonoBehaviour {
+
+    public GameObject inGameMenuObject;
+
+    // Use this for initialization
+    void Start () {
+        Debug.Log("DungeonController script started.");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        // Enables in-game menu when Esc key pressed
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            inGameMenuObject.SetActive(true);
+        }
+    }
+
+    public void QuitMatch()
+    {
+        Debug.Log("MATCH QUIT");
+        SceneManager.LoadScene(0);
+    }
+}
