58d1f68f141a45e8f08290c73e35d24cf005ae8c
SusanSuY <yuens2@mcmaster.ca>
2018-10-27 22:38:51 -0400

    Update hero prefab and hero scripts. Added colliders, updated hero movement, updated hero camera movement

--- TPK/Assets/Scripts/HeroCameraController.cs
+++ TPK/Assets/Scripts/HeroCameraController.cs
@@ -3,26 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-//Cameras should only be spawned locally?
-public class HeroCameraController : MonoBehaviour {
+// HeroCameraController: camera spawned when hero prefab is spawned (HeroCamera is instantiated in HeroController.cs)
+// Cameras should only be spawned locally?
+public class HeroCameraController : MonoBehaviour
+{
 
     private Transform targetTransform;
     private float smoothspeed = 0.5f;
     private Vector3 offset = new Vector3(0, 8, -5);
 
-	// Use this for initialization
-	void Start () {
-        this.transform.position = offset;
-	}
-    void LateUpdate( )
+    // Use this for initialization
+    void Start()
     {
-        Debug.Log("should be following hero");
-        Vector3 desired = targetTransform.position + offset;
-        Vector3 smoothed = Vector3.Lerp(this.transform.position, desired, smoothspeed);
-        transform.position = smoothed;
+        this.transform.position = offset;
     }
 
-    public void setTarget(Transform target) {
+    void Update()
+    {
+        Vector3 desired = targetTransform.position + offset;
+        //Vector3 smoothed = Vector3.Lerp(this.transform.position, desired, smoothspeed);
+        transform.position = desired;   // desired does not cause camera stutter when character is moving -Susan
+    }
+
+    public void setTarget(Transform target)
+    {
         targetTransform = target;
     }
 }
--- TPK/Assets/Scripts/HeroController.cs
+++ TPK/Assets/Scripts/HeroController.cs
@@ -3,30 +3,58 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 
-//Attacker Controller Script for controlling character.
-public class HeroController : NetworkBehaviour {
-
+// HeroController: script for controlling attacker/hero character.
+public class HeroController : NetworkBehaviour
+{
 
     public GameObject heroCam;
     private GameObject cam;
+    private float moveSpeed;
+    private Rigidbody heroRigidbody;
 
-	// Use this for initialization
-	void Start () {
-        if (!hasAuthority) return;
-        startCamera();
+    // Use this for initialization
+    void Start()
+    {
+        if (!hasAuthority)
+        {
+            return;
+        }
+
+        moveSpeed = 5.0f;
+        heroRigidbody = GetComponent<Rigidbody>();
+
+        StartCamera();
     }
-	
-	// Update is called once per frame
-	void Update () {
-        ////This function runs on all hero's
-        if (!hasAuthority) return;
-        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 5.0f;
-        var z = Input.GetAxis("Vertical") * Time.deltaTime * 5.0f;
-        transform.Translate(x, 0, z);
 
+    // Update is called once per frame
+    void Update()
+    {
+        // This function runs on all heroes
+
+        if (!hasAuthority)
+        {
+            return;
+        }
+
+        CharacterMovement();
     }
 
-    private void startCamera() {
+    // Hero character movement
+    private void CharacterMovement()
+    {
+        if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
+        {
+            heroRigidbody.velocity = new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed, 0, Input.GetAxisRaw("Vertical") * moveSpeed);
+        }
+        else
+        {
+            heroRigidbody.velocity = new Vector3(0, 0, 0);
+        }
+    }
+
+    private void StartCamera()
+    {
+        Debug.Log("StartCamera called.");
         GameObject.FindGameObjectWithTag("MainCamera").SetActive(false);
         cam = Instantiate(heroCam);
         cam.GetComponent<HeroCameraController>().setTarget(this.transform);
