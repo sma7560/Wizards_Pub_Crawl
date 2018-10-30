140ba5af07fa52144372687cdf036650daca66d8
Thien Trandinh <chaos449@gmail.com>
2018-10-30 19:59:48 -0400

    Updated network manager to allow for lan connection from different systems.

--- TPK/Assets/Scripts/NetworkManagerExtension.cs
+++ TPK/Assets/Scripts/NetworkManagerExtension.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Networking;
+using System.Net;
+using System.Net.Sockets;
+using System;
 
 public class NetworkManagerExtension : NetworkManager{
 
     public void StartUpHost() {
         SetPort();
+        networkAddress = GetLocalIPAddress();
+        Debug.Log("Hosting on " + networkAddress);
         NetworkServer.Reset();
         NetworkManager.singleton.StartHost();
     }
@@ -24,8 +29,22 @@ public class NetworkManagerExtension : NetworkManager{
         if (ipAddress == null) ipAddress = "localhost";
 
         NetworkManager.singleton.networkAddress = ipAddress;
-
     }
+
+    //get host IP Address
+    public static string GetLocalIPAddress()
+    {
+        var host = Dns.GetHostEntry(Dns.GetHostName());
+        foreach (var ip in host.AddressList)
+        {
+            if (ip.AddressFamily == AddressFamily.InterNetwork)
+            {
+                return ip.ToString();
+            }
+        }
+        throw new Exception("Local IP Address Not Found!");
+    }
+
     private void SetPort()
     {
         NetworkManager.singleton.networkPort = 7777;
