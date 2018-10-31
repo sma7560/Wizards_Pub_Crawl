56142d55ae610d913e7413c4e9d55d7ef4df3d5a
sma7560 <sma7560@gmail.com>
2018-10-31 14:57:11 -0400

    Started cards using scriptable objects
    SM

---
+++ TPK/Assets/Scripts/Cards/DefenderCard.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Scriptable object that all cards will extend
+[CreateAssetMenu(fileName = "New Card", menuName = "Defender Card")]
+public class DefenderCard : ScriptableObject {
+
+	public string cardName;
+	public string desc;
+
+	public Sprite artwork;
+
+	public int costEnergy;
+	public int costMoney;
+}
