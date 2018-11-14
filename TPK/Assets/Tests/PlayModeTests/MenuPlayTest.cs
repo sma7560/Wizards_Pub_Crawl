12d21c02185de69bfd0b341bc014611e14e1f3b0
SusanSuY <yuens2@mcmaster.ca>
2018-11-14 00:39:52 -0500

    Add test folders and one menu system test. Also added Menu -> DungeonLevel scenes in Build settings (required for test to run)

---
+++ TPK/Assets/Tests/PlayModeTests/MenuPlayTest.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuPlayTest
+{
+    readonly string testSceneName = "TestScene";
+    readonly string menuSceneName = "Menu";
+
+    [SetUp]
+    public void SetUp()
+    {
+        // Load menu scene
+        SceneManager.LoadSceneAsync(menuSceneName, LoadSceneMode.Single);
+    }
+
+    /// <summary>
+    /// Test M1: Checks that the main menu is the only active menu upon initial launch of menu scene
+    /// Requirement: FR-M1
+    /// </summary>
+    [UnityTest]
+    public IEnumerator Menu_MenusExist()
+    {
+        // Wait for menu scene to be loaded
+        yield return new WaitForSeconds(3);
+
+        // Get all menus
+        GameObject mainMenu = GameObject.Find("MainMenu");
+        GameObject settingsMenu = GameObject.Find("SettingsMenu");
+        GameObject startGameMenu = GameObject.Find("StartGameMenu");
+        GameObject newMatchMenu = GameObject.Find("NewMatchMenu");
+        GameObject joinMatchMenu = GameObject.Find("JoinMatchMenu");
+        GameObject howToPlayMenu = GameObject.Find("HowToPlayMenu");
+
+        // Assertions; only main menu should be not null (only one active)
+        Assert.IsNotNull(mainMenu);
+        Assert.IsNull(settingsMenu);
+        Assert.IsNull(startGameMenu);
+        Assert.IsNull(newMatchMenu);
+        Assert.IsNull(joinMatchMenu);
+        Assert.IsNull(howToPlayMenu);
+
+        yield return null;
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        // Unload menu scene
+        SceneManager.UnloadSceneAsync(menuSceneName);
+    }
+}
