c7e77b2e13c2f7e97e4173e136a5a1891b64e279
Zihao Chen <chenz87@mcmaster.ca>
2018-11-26 00:36:57 -0500

    add animation test scene

---
+++ TPK/Assets/Scripts/Animation/Acontroller.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Acontroller : MonoBehaviour {
+	static Animator anim;
+	public float speed = 10.0F;
+	public float rotationSpeed = 100.0F;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float translation = Input.GetAxis("Vertical") * speed;
+		float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+		translation += Time.deltaTime;
+		rotation += Time.deltaTime;
+		transform.Translate(0, 0, translation);
+		transform.Rotate(0, rotation, 0);
+		if(Input.GetButtonDown("Jump")){
+			anim.SetTrigger("punch");
+		}
+
+		if(translation != 0){
+			anim.SetBool("isRunning", true);
+		}
+		else{
+			anim.SetBool("isRunning", false);
+		}
+		
+	}
+} 
