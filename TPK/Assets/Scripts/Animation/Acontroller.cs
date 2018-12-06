b51c55130cf73b6dec6205e1c123f752db7d4778
Zihao Chen <chenz87@mcmaster.ca>
2018-12-06 17:37:51 -0500

    ,Movement is done, toggle shift to run, jkL Space to atk

--- TPK/Assets/Scripts/Animation/Acontroller.cs
+++ TPK/Assets/Scripts/Animation/Acontroller.cs
@@ -9,41 +9,78 @@ public class Acontroller : MonoBehaviour {
 	public float rotationSpeed = 1.0F;*/
     private float inputH;
     private float inputV;
-    
+    private bool run;
 
     // Use this for initialization
     void Start () {
 		anim = GetComponent<Animator>();
         rbody = GetComponent<Rigidbody>();
+        run = false;
     }
 	
 	// Update is called once per frame
 	void Update () {
 
-        /*float translation = Input.GetAxis("Vertical") * speed;
-        
+        /*This is for top down angle with 360 degree rotation
+        float translation = Input.GetAxis("Vertical") * speed;
         float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
 		translation += Time.deltaTime;
 		rotation += Time.deltaTime;
 		transform.Translate(0, 0, translation);
 		transform.Rotate(0, rotation, 0);*/
-        bool walkF = Input.GetKey("w");
-        anim.SetBool("isWalking", walkF);
-        bool hitPressed = Input.GetKey("space");
-        anim.SetBool("isHit", hitPressed);
+        //bool walkF = Input.GetKey("w");
+        //anim.SetBool("isWalking", walkF);
+        //bool hitPressed = Input.GetKey("space");
+        //anim.SetBool("isHit", hitPressed);
+
+        // Matk for magic atk, P for physics atk
         if (Input.GetKeyDown("j"))
         {
-            anim.Play("atk2", -1, 0f);
+            anim.Play("Matk1", -1, 0f);
         }
-        
+
+        if (Input.GetKeyDown("k"))
+        {
+            anim.Play("Matk2", -1, 0f);
+        }
+        //Swing
+        if (Input.GetKey("space"))
+        {
+            anim.Play("Patk1", -1, 0f);
+        }
+        //Kick
+        if (Input.GetKey("l"))
+        {
+            anim.Play("Patk2", -1, 0f);
+        }
+
         inputH = Input.GetAxis("Horizontal");
         inputV = Input.GetAxis("Vertical");
         anim.SetFloat("inputH", inputH);
         anim.SetFloat("inputV", inputV);
+        anim.SetBool("run", run);
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            run = true;
+        }
+        else
+        {
+            run = false;
+        }
         float moveX = inputH * 20f * Time.deltaTime;
         float moveZ = inputV * 50f * Time.deltaTime;
+
+        if(moveZ <= 0f)
+        {
+            moveX = 0f;
+        }
+        else if(run)
+        {
+            moveX *= 3f;
+            moveZ *= 3f;
+        }
         transform.Translate(0, 0, 0.01f * moveZ);
-        //rbody.velocity = new Vector3(moveX, 0f, moveZ);
+        //rbody.velocity = new Vector3(moveX, 0f, 1f * moveZ);
 
     }
 } 
