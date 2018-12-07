f32ac90f2aa1144dd86409e374b7e54a08eb54f5
SusanSuY <yuens2@mcmaster.ca>
2018-12-07 10:42:06 -0500

    Add score script

---
+++ TPK/Assets/Scripts/Attacker/Score.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This class holds information regarding the score of the player.
+/// </summary>
+public class Score
+{
+    private int score;
+
+    public Score()
+    {
+        // Initialize score to 0
+        score = 0;
+    }
+
+    public int GetScore()
+    {
+        return score;
+    }
+
+    // Increment the score by a value
+    public void IncreaseScore(int increment)
+    {
+        score += increment;
+    }
+
+}
