df9fd17b705826b9a6214b48aea4a491d351493a
Thien Trandinh <chaos449@gmail.com>
2019-02-28 23:44:55 -0500

    Added item interface class, with healthitem inheriting from it. Added health pick up prefab implementing item interface

---
+++ TPK/Assets/Scripts/Player-General/Item.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+/*
+ * Interface class for items
+ */
+public class Item : NetworkBehaviour
+{
+    //check if player comes in contact with item
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            ItemConsume(other);
+        }
+        Destroy(gameObject);
+    }
+
+    //item effect. Will be override by specific item effects
+    public virtual void ItemConsume(Collider other)
+    {
+        //child classes will define this function
+    }
+}
