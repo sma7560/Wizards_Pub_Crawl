cbee344c01df2d256c2e39d260b93fd1124bb7b2
Thien Trandinh <chaos449@gmail.com>
2019-03-01 22:23:11 -0500

    Added invisible walls that allows player, but not enemies through. These walls are situated outside spawn rooms of players

---
+++ TPK/Assets/Scripts/Game-Management/EnemyNoEntry.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Function to allow player to pass through invisible wall that prevents enemies from entering
+public class EnemyNoEntry : MonoBehaviour {
+
+    void OnCollisionEnter(Collision collision)
+    {
+        //allows players to pass through
+        if (collision.gameObject.tag == "Player")
+        {
+            Physics.IgnoreCollision(collision.collider, GetComponent<Collider>());
+        }
+    }
+}
