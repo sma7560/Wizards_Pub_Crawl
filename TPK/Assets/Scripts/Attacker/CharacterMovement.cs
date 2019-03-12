d68c32155493a7d4d49c96dccb8c9d65a955919d
Ethan Chan <chaneh@mcmaster.ca>
2019-03-12 14:57:12 -0400

    Normalizing movement vector to give equal speed all around

--- TPK/Assets/Scripts/Attacker/CharacterMovement.cs
+++ TPK/Assets/Scripts/Attacker/CharacterMovement.cs
@@ -22,11 +22,18 @@ public class CharacterMovement
         }
     }
 
+    // Mathematically speaking it is now correct.
     public Vector3 Calculate(float x, float z)
     {
         if (x != 0 || z != 0)
         {
-            return new Vector3(x * speed, 0, z * speed);
+            float mag = Mathf.Sqrt((x * x) + (z * z));
+            Vector3 dir = new Vector3(x/mag, 0, z/mag);
+            //Debug.Log(dir.magnitude);
+            //Debug.Log(dir.ToString());
+            return speed * dir.normalized;
+
+            //return new Vector3(x * speed, 0, z * speed);
         }
 
         return new Vector3(0, 0, 0);
