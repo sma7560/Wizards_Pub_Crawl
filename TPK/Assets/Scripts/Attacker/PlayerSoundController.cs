6793cff9ae0922ab2c00803a629f4a74e404a303
Thien Trandinh <chaos449@gmail.com>
2019-03-14 18:34:29 -0400

    Added sound effects for skills.

---
+++ TPK/Assets/Scripts/Attacker/PlayerSoundController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSoundController : MonoBehaviour {
+
+    private AudioSource source;
+    private AudioClip deathBall;
+
+    public enum Skills { DeathBall };
+
+	// Use this for initialization
+	void Start () {
+        source = GetComponent<AudioSource>();
+        //load deathball sound effect from resource folder
+        deathBall = Resources.Load("SoundEffects/DeathBall") as AudioClip;
+	}
+	
+    //plays different skill effects
+	public void playDeathBallSoundEffect()
+    {
+         source.PlayOneShot(deathBall);
+    }
+}
