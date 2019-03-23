ef161d6bfded9eb92b601d092a254365d18d9bba
SusanSuY <yuens2@mcmaster.ca>
2019-03-23 00:08:21 -0400

    Change implementation of #190 fix to something more simple

--- TPK/Assets/Scripts/UI/MainMenuController.cs
+++ TPK/Assets/Scripts/UI/MainMenuController.cs
@@ -1,9 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 /// <summary>
@@ -23,25 +21,5 @@ public class MainMenuController : MonoBehaviour
     {
         Debug.Log("QUIT");
         Application.Quit();
-    }
-
-    /// <summary>
-    /// Disables all buttons while new match is being created.
-    /// This reduces the chances of the user clicking the "New Match" button twice, which can cause glitches.
-    /// </summary>
-    public void CreatingNewMatch()
-    {
-        // Replace text with "CREATING MATCH..."
-        TextMeshProUGUI joinMatchText = GameObject.Find("JoinMatchText").GetComponent<TextMeshProUGUI>();
-        joinMatchText.text = "CREATING MATCH...";
-
-        // Disable all other buttons
-        GameObject newMatchButton = GameObject.Find("HostButton");
-        Button joinMatchButton = GameObject.Find("JoinMatchButton").GetComponent<Button>();
-        GameObject backButton = GameObject.Find("BackButton");
-        newMatchButton.SetActive(false);
-        joinMatchButton.interactable = false;
-        EventSystem.current.SetSelectedGameObject(null);
-        backButton.SetActive(false);
     }
 }
