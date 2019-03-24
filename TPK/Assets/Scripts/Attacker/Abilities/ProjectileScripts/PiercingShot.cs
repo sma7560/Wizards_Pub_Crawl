676ca3e5f4607b73a5a8f75ce1b55a3fbe87b53d
sma7560 <sma7560@gmail.com>
2019-03-24 03:17:31 -0400

    Update PiercingShot.cs
    Piercing shot should now be unable to penetrate walls, only enemies.

--- TPK/Assets/Scripts/Attacker/Abilities/ProjectileScripts/PiercingShot.cs
+++ TPK/Assets/Scripts/Attacker/Abilities/ProjectileScripts/PiercingShot.cs
@@ -21,18 +21,21 @@ public class PiercingShot : MonoBehaviour {
 		switch (col.transform.tag)
 		{
 			case "Enemy":
-			if (col.transform.GetComponent<EnemyStats>())
-				{
-					// This will change.
-				col.transform.GetComponent<EnemyStats>().CmdTakeDamage(damage);
-				}
+				if (col.transform.GetComponent<EnemyStats>())
+					{
+						// This will change.
+					col.transform.GetComponent<EnemyStats>().CmdTakeDamage(damage);
+					}
 				break;
 			case "Player":
-			if (col.transform.GetComponent<HeroModel>())
-				{
-				col.transform.GetComponent<HeroModel>().CmdTakeDamage(damage, damageType);
-				}
+				if (col.transform.GetComponent<HeroModel>())
+					{
+					col.transform.GetComponent<HeroModel>().CmdTakeDamage(damage, damageType);
+					}
 				// This means you can shoot yourself... so far.
+				break;
+			default:
+				Destroy (gameObject);
 				break;
 		}
 	}
