aa9ba90600465ef8488c12faf089d44c796232a1
SusanSuY <yuens2@mcmaster.ca>
2019-03-27 20:48:39 -0400

    Added slider for sound effects volume

--- TPK/Assets/Scripts/Game-Management/AudioManager.cs
+++ TPK/Assets/Scripts/Game-Management/AudioManager.cs
@@ -7,85 +7,188 @@ using UnityEngine;
 /// </summary>
 public static class AudioManager
 {
-    private static float volume = 1.0f;
-    private static bool mute = false;
+    // Background volume level
+    private static float bgVolume = 1.0f;
+    private static bool bgMute = false;
+
+    // Sound effect volume level
+    private static float sfxVolume = 1.0f;
+    private static bool sfxMute = false;
 
     /// <returns>
-    /// Returns the current volume of the system.
+    /// Returns the current background volume of the system.
     /// </returns>
-    public static float GetVolume()
+    public static float GetBgVolume()
     {
-        if (mute)
+        if (bgMute)
         {
             return 0;
         }
 
-        return volume;
+        return bgVolume;
     }
 
     /// <returns>
-    /// Returns the set volume ignoring mute.
+    /// Returns the current sound effects volume of the system.
     /// </returns>
-    public static float GetVolumeIgnoreMute()
+    public static float GetSfxVolume()
     {
-        return volume;
+        if (sfxMute)
+        {
+            return 0;
+        }
+
+        return sfxVolume;
+    }
+
+    /// <returns>
+    /// Returns the current background volume ignoring mute.
+    /// </returns>
+    public static float GetBgVolumeIgnoreMute()
+    {
+        return bgVolume;
+    }
+
+    /// <returns>
+    /// Returns the current sound effects volume ignoring mute.
+    /// </returns>
+    public static float GetSfxVolumeIgnoreMute()
+    {
+        return sfxVolume;
     }
 
     /// <summary>
-    /// Sets the volume to the desired value. Assumes that AudioSource is always contained in "EventSystem" GameObject.
+    /// Sets the background volume to the desired value.
+    /// Assumes that AudioSource is always contained in "EventSystem" GameObject.
     /// </summary>
     /// <param name="vol">Desired volume setting.</param>
-    public static void SetVolume(float vol)
+    public static void SetBgVolume(float vol)
     {
-        volume = vol;
+        bgVolume = vol;
 
-        if (!mute)
+        if (!bgMute)
         {
-            SetSystemVolume(volume);
+            SetSystemBgVolume(bgVolume);
         }
     }
 
     /// <summary>
-    /// Sets whether or not the audio is muted.
+    /// Sets the sound effects volume to the desired value.
+    /// Assumes that AudioSource is always contained in this player's GameObject.
     /// </summary>
-    /// <param name="shouldMute">If audio should be muted or not.</param>
-    public static void SetMute(bool shouldMute)
+    /// <param name="vol">Desired volume setting.</param>
+    /// <param name="player">The player's GameObject.</param>
+    public static void SetSfxVolume(float vol)
     {
-        mute = shouldMute;
-        if (mute)
+        sfxVolume = vol;
+
+        if (!sfxMute)
         {
-            SetSystemVolume(0f);
+            SetSystemSfxVolume(sfxVolume);
+        }
+    }
+
+    /// <summary>
+    /// Sets whether or not the background audio is muted.
+    /// </summary>
+    /// <param name="shouldMute">If background audio should be muted or not.</param>
+    public static void SetBgMute(bool shouldMute)
+    {
+        bgMute = shouldMute;
+        if (bgMute)
+        {
+            SetSystemBgVolume(0f);
         }
         else
         {
-            SetSystemVolume(volume);
+            SetSystemBgVolume(bgVolume);
+        }
+    }
+
+    /// <summary>
+    /// Sets whether or not the sound effects audio is muted.
+    /// </summary>
+    /// <param name="shouldMute">If sound effects audio should be muted or not.</param>
+    public static void SetSfxMute(bool shouldMute)
+    {
+        sfxMute = shouldMute;
+        if (sfxMute)
+        {
+            SetSystemSfxVolume(0f);
+        }
+        else
+        {
+            SetSystemSfxVolume(sfxVolume);
         }
     }
 
     /// <returns>
-    /// Returns whether or not the audio is muted.
+    /// Returns whether or not the background audio is muted.
     /// </returns>
-    public static bool GetMute()
+    public static bool GetBgMute()
     {
-        return mute;
+        return bgMute;
+    }
+
+    /// <returns>
+    /// Returns whether or not the sound effects audio is muted.
+    /// </returns>
+    public static bool GetSfxMute()
+    {
+        return sfxMute;
     }
 
     /// <summary>
-    /// Set actual game volume.
+    /// Set actual game background volume.
     /// Assume AudioSource is in "EventSystem" GameObject.
     /// </summary>
     /// <param name="volume">Volume to set the system audio to.</param>
-    public static void SetSystemVolume(float volume)
+    public static void SetSystemBgVolume(float volume)
     {
         if (GameObject.Find("EventSystem").GetComponent<AudioSource>() != null)
         {
-            if (mute)
+            if (bgMute)
             {
                 GameObject.Find("EventSystem").GetComponent<AudioSource>().volume = 0f;
             }
             else
             {
                 GameObject.Find("EventSystem").GetComponent<AudioSource>().volume = volume;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Set actual game sound effects volume.
+    /// Assume AudioSource is in player's GameObject.
+    /// </summary>
+    /// <param name="volume">Volume to set the system audio to.</param>
+    public static void SetSystemSfxVolume(float volume)
+    {
+        // For main menu sound effects
+        if (GameObject.Find("SfxAudio") != null)
+        {
+            if (sfxMute)
+            {
+                GameObject.Find("SfxAudio").GetComponent<AudioSource>().volume = 0f;
+            }
+            else
+            {
+                GameObject.Find("SfxAudio").GetComponent<AudioSource>().volume = volume;
+            }
+        }
+
+        // For sound effects attached to players
+        GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+        foreach (GameObject player in players)
+        {
+            if (sfxMute)
+            {
+                player.GetComponent<AudioSource>().volume = 0f;
+            }
+            else
+            {
+                player.GetComponent<AudioSource>().volume = volume;
             }
         }
     }
