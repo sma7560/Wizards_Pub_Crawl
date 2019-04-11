9fe8fd254a9eede09c31184ac09a2b61acdc785e
Simon Ma <sma7560@gmail.com>
2019-04-11 17:08:53 -0400

    Update SkillEffectSFX.cs
    Added comments

--- TPK/Assets/Resources/Skill Effects/SkillEffectSFX.cs
+++ TPK/Assets/Resources/Skill Effects/SkillEffectSFX.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Plays a sound effect on creation.
+/// GameObject is kept alive for duration of the audio clip.
+/// </summary>
 public class SkillEffectSFX : MonoBehaviour {
 
 	public AudioClip sfx;
@@ -13,9 +17,5 @@ public class SkillEffectSFX : MonoBehaviour {
 		audSrc.PlayOneShot(sfx);
 		Destroy (gameObject, sfx.length);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
 }
