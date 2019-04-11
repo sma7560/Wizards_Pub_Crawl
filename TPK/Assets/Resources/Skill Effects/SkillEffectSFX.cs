e2dded7f03596fe9f0ad1bdd3ae2e76500476811
Simon Ma <sma7560@gmail.com>
2019-04-11 16:10:13 -0400

    Added impact sound for Fireball
    Created a script to attach to particle effect that keeps it alive long enough to finish the audio clip.

---
+++ TPK/Assets/Resources/Skill Effects/SkillEffectSFX.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillEffectSFX : MonoBehaviour {
+
+	public AudioClip sfx;
+	private AudioSource audSrc;
+
+	// Use this for initialization
+	void Start () {
+		audSrc = GetComponent<AudioSource> ();
+		audSrc.PlayOneShot(sfx);
+		Destroy (gameObject, sfx.length);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
