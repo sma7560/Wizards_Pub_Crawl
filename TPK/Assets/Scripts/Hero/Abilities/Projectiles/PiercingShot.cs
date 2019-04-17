5746c49594d9e7829b25925cb7f79cba2a60c6bf
sma7560 <sma7560@gmail.com>
2019-04-16 21:34:50 -0400

    Added impact effect and sound effect for piercing shot

--- TPK/Assets/Scripts/Hero/Abilities/Projectiles/PiercingShot.cs
+++ TPK/Assets/Scripts/Hero/Abilities/Projectiles/PiercingShot.cs
@@ -1,10 +1,13 @@
 ﻿using UnityEngine;
+using UnityEngine.Networking;
 
 /// <summary>
 /// Projectile behaviour for Piercing Shot skill.
 /// </summary>
 public class PiercingShot : BaseProjectile
 {
+	public GameObject impactFX;
+
     /// <summary>
     /// The piercing shot only destroys on things that are not walls/environment objects.
     /// </summary>
@@ -12,19 +15,21 @@ public class PiercingShot : BaseProjectile
     {
         switch (col.transform.tag)
         {
-            case "Enemy":
-                // Damage enemy
-                if (col.transform.GetComponent<EnemyModel>() != null)
-                {
-                    col.transform.GetComponent<EnemyModel>().CmdTakeDamage(damage);
-                }
+			case "Enemy":
+	                // Damage enemy
+				if (col.transform.GetComponent<EnemyModel> () != null)
+				{
+					col.transform.GetComponent<EnemyModel> ().CmdTakeDamage (damage);
+				}
+				CmdEffect ();
                 break;
-            case "Player":
-                // Damage player
-                if (col.transform.GetComponent<HeroModel>() != null)
-                {
-                    col.transform.GetComponent<HeroModel>().CmdTakeDamage(damage);
-                }
+			case "Player":
+	                // Damage player
+				if (col.transform.GetComponent<HeroModel> () != null)
+				{
+					col.transform.GetComponent<HeroModel> ().CmdTakeDamage (damage);
+				}
+				CmdEffect ();
                 break;
             case "Projectile":
                 // Pierces projectiles and shields
@@ -34,4 +39,15 @@ public class PiercingShot : BaseProjectile
                 break;
         }
     }
+
+	/// <summary>
+	/// Plays the explosion effect.
+	/// </summary>
+	[Command]
+	private void CmdEffect()
+	{
+		GameObject effect = Instantiate(impactFX);
+		effect.transform.position = transform.position;
+		NetworkServer.Spawn(effect);
+	}
 }
