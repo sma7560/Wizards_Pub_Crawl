1153768d255bb05c4e520cc61f509c739533c0ee
sma7560 <sma7560@gmail.com>
2019-04-21 03:11:10 -0400

    Updated Flame Wall
    Updated to match new particle collision system

--- TPK/Assets/Scripts/Hero/Abilities/Projectiles/FlameWall.cs
+++ TPK/Assets/Scripts/Hero/Abilities/Projectiles/FlameWall.cs
@@ -5,60 +5,32 @@
 /// </summary>
 public class FlameWall : BaseProjectile
 {
-    private readonly float dmgDelay = 0.1f;     // cooldown of damage done by flame wall
-    private float nextActiveTime;               // the next time where flame wall will do damage
-
-    void Awake()
-    {
-        nextActiveTime = 0;
-    }
-
-    void Update()
-    {
-        // Update next active time
-        if (Time.time > nextActiveTime)
-        {
-            nextActiveTime = Time.time + dmgDelay;
-        }
-    }
-
-	//Damage for the first time they enter
-	void OnTriggerEnter(Collider col)
-	{
+	void OnParticleCollision(GameObject col){
+		if (!isServer) return;
 		Behaviour (col);
 	}
 
-    /// <summary>
-    /// Damage over time if enemy/player stays within the flame wall.
-    /// </summary>
-    void OnTriggerStay(Collider col)
-    {
-        if (Time.time < nextActiveTime) return;
-
-		Behaviour (col);
-    }
-
-    /// <summary>
-    /// Initial damage when enemy/player touches flame wall.
-    /// </summary>
-    public override void Behaviour(Collider col)
-    {
-        switch (col.transform.tag)
-        {
-            case "Enemy":
-                // Damage enemy
-                if (col.transform.GetComponent<EnemyModel>() != null)
-                {
-                    col.transform.GetComponent<EnemyModel>().CmdTakeDamage(damage);
-                }
-                break;
-            case "Player":
-                // Damage player
-                if (col.transform.GetComponent<HeroModel>() != null)
-                {
-                    col.transform.GetComponent<HeroModel>().CmdTakeDamage(damage);
-                }
-                break;
-        }
-    }
+	/// <summary>
+	/// Initial damage when enemy/player touches the fire.
+	/// </summary>
+	public void Behaviour(GameObject col)
+	{
+		switch (col.transform.tag)
+		{
+		case "Enemy":
+			// Damage enemy
+			if (col.transform.GetComponent<EnemyModel>() != null)
+			{
+				col.transform.GetComponent<EnemyModel>().CmdTakeDamage(damage);
+			}
+			break;
+		case "Player":
+			// Damage player
+			if (col.transform.GetComponent<HeroModel>() != null)
+			{
+				col.transform.GetComponent<HeroModel>().CmdTakeDamage(damage);
+			}
+			break;
+		}
+	}
 }
